a7a132cb7922a865f2e72ec56055a3384c33e5f3
Ayush Garg <84924734+gargayush909@users.noreply.github.com>
2023-08-06 11:05:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"
+let name;
+let age=null;
+console.log(name) // output is undeifined
+console.log(age); //output is null
+
+// null is object 
+// undefined is a type
+// ye pta chla typeof( ka use kr k )
