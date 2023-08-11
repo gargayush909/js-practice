6befe7e30736e9ecae17ca943b34b7688edf579f
Ayush Garg <84924734+gargayush909@users.noreply.github.com>
2023-08-11 11:03:41 +0000

    summary data-type

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+//2 types k data stypes 
+ 
+//7 primitive vhi normal boolean, number, symbol, undefined, null, Symbol, String, BigInt
+//3 non premitive ya primitive vhi normaL
+
+const id = Symbol('123')
+const anotherID= Symbol('123')
+
+console.log(id === anotherID)
+
+const heroes= ["Aloo", "Tamatar", "khira"]; //array ki declaration
+
+let myObject= {
+    name: "Ayush",              //Object ki declaration
+    age: "20",
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
