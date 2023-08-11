c2cf68067940cf11967ccf60b3fd50711b4a7851
Ayush Garg <84924734+gargayush909@users.noreply.github.com>
2023-08-11 08:52:08 +0000

    conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,14 @@
+let score="33"; // agr bina coutes k likhta to no. he rhta 
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueofNumber= Number(score)
+// hmesha data-type ka pehla letter large likhna h 
+
+console.log(typeof valueofNumber)
+//"1" == True
+//"0" == False 
+//""== false
+//"Ayush"== true 
+// number k age string likh denge to vo bhi string m covert ho jayega 
