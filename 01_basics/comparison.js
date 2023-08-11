cf75270e4ee4436ad5d9dccc5b0b523dd4c4008b
Ayush Garg <84924734+gargayush909@users.noreply.github.com>
2023-08-11 09:26:52 +0000

    comparison in data type

---
+++ 01_basics/comparison.js
@@ -0,0 +1,10 @@
+console.log(null>0)
+console.log(null == 0)
+console.log(null>= 0) // convert krdete h comprison wale null ko numbe rm , or null number mai 0 hota hai 
+
+
+console.log(undefined >0)
+console.log(undefined == 0)
+console.log(undefined >= 0) 
+
+console.log("2" === 2) // ye h strict check , data type bhi check hoga , koi conversion nhi h 
